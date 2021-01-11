bba9b67b849caf438adf1e16799af156c69fd289
Fellipe Rezende <rezenfellipe9@gmail.com>
2021-01-11 12:13:57 -0300

    Add_Month_Availability_function

--- client/src/hooks/auth.tsx
+++ client/src/hooks/auth.tsx
@@ -42,6 +42,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -58,6 +60,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
